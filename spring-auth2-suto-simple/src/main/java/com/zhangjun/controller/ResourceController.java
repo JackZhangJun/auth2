7a641c06af4cebfb06bd80f3443f2608bc5f1a2f
张军 <zhangjun@tuya.com>
2020-12-26 17:33:59 +0800

    auth2 first commit

---
+++ spring-auth2-suto-simple/src/main/java/com/zhangjun/controller/ResourceController.java
@@ -0,0 +1,18 @@
+package com.zhangjun.controller;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RestController
+public class ResourceController {
+
+    @GetMapping("/resource")
+    public HttpEntity<?> resource(Principal principal)
+    {
+        return ResponseEntity.ok(principal);
+    }
+}
